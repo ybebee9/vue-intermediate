8e4713643c2b031137d101aae8b99dc7c2afaaa6
ybebee9 <ybebee9@gmail.com>
2020-03-11 23:49:39 +0900

    [리팩토링] state 속성 적용

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -41,15 +41,6 @@ export default {
       this.todoItems = [];
     }
   },
-  created () {
-    if(localStorage.length > 0){
-      for (let i = 0; i < localStorage.length; i++){
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-  },
   components: {
     TodoHeader,
     TodoInput,
